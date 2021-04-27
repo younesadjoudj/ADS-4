f216189fea52470a378c98652c2dd216b9e435a9
anton <ashtanyuk@gmail.com>
2021-04-27 15:03:18 +0300

    Исправления для cpplint

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,9 @@
 
 int main() {
     TPQueue<SYM> pqueue;
-    pqueue.push(SYM{'a',4});
-    pqueue.push(SYM{'b',7});
-    SYM c1=pqueue.pop();
-    SYM c2=pqueue.pop();
+    pqueue.push(SYM{'a', 4});
+    pqueue.push(SYM{'b', 7});
+    SYM c1 = pqueue.pop();
+    SYM c2 = pqueue.pop();
     return 0;
 }
