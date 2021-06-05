1b31b5312a98fb7c0ee1ef4b385ef3e6d5d7479a
younesadjoudj <79196405+younesadjoudj@users.noreply.github.com>
2021-06-06 00:53:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,1 +1,67 @@
 // Copyright 2021 NNTU-CS
+#ifndef INCLUDE_TPQUEUE_H_
+#define INCLUDE_TPQUEUE_H_
+#include <cassert>
+template<typename T>
+class TPQueue {
+ private:
+    T* arr;
+    int size;
+    int begin, end;
+    int count;
+ public:
+    TPQueue();
+    ~TPQueue();
+    void push(const T&);
+    T pop();
+    T get() const;
+    bool isEmpty() const;
+    bool isFull() const;
+};
+
+template<typename T>
+TPQueue<T>::TPQueue() : size(100),
+    begin(0), end(0), count(0) {
+    arr = new T[size + 1];
+}
+
+template<typename T>
+TPQueue<T>::~TPQueue() {
+    delete[] arr;
+}
+
+template<typename T>
+void TPQueue<T>::push(const T& item) {
+    assert(count < size);
+    if (end != 0) {
+        for (int i = end - 1; i > -1; i--) {
+            if (arr[i].prior >= item.prior) {
+                arr[i + 1] = item;
+                continue;
+            }
+            if (arr[i].prior < item.prior) {
+                arr[i + 1] = arr[i];
+              if (i == 0) {
+                    arr[i] = item;
+                }
+                    continue;
+            }
+        }
+    } else {
+        arr[begin] = item;
+    }
+    count++;
+    end++;
+    if (end > size) {
+        end -= size + 1;
+    }
+}
+
+template<typename T>
+T TPQueue<T>::pop() {
+    assert(count > 0);
+    T item = arr[begin++];
+    count--;
+    if (begin > size) {
+        begin -= size + 1;
+    }
